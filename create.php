7b3f631809a9b45593d2fd6597b6adda0644ac28
kunfirekun <okello_daniel@hotmail.com>
2022-05-06 15:46:11 +0300

    commit on branch

--- create.php
+++ create.php
@@ -119,7 +119,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
-            width: 600px;
+            width: 100%;
             margin: 0 auto;
         }
     </style>
